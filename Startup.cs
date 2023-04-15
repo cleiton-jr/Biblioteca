7473813c297b44a57ee7b967dadb1f7b78b8af1d
cleiton-jr <cleiton.almeida13@etec.sp.gov.br>
2023-04-14 22:22:12 -0300

    14-4-23

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,6 @@ namespace Biblioteca
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             services.AddControllersWithViews();
             services.AddDistributedMemoryCache();
             services.AddSession();
@@ -35,13 +34,23 @@ namespace Biblioteca
             {
                 app.UseDeveloperExceptionPage();
             }
-            
-            app.UseStaticFiles();
-            app.UseMvc(routes =>
+            else
             {
-                routes.MapRoute(
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseSession();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action-login}/{id?}";
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
